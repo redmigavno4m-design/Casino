1d6c0e43c1c7229641442747f5bbed71ba009266
redmigavno4m-design <redmigavno4m@gmail.com>
2026-09-12 03:16:10 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+# Настройки казино
+START_BALANCE = 1000
+MIN_BET = 50
+
+# Слоты
+SLOT_SYMBOLS = ['🍒', '🍋', '🍊', '🍇', '⭐', '💎', '7️⃣']
+SLOT_WEIGHTS = [25, 25, 20, 15, 8, 5, 2]
+SLOT_PAYOUTS = {'7️⃣': 40, '💎': 20, '⭐': 12, '🍇': 8, '🍊': 5, '🍋': 4, '🍒': 3}
+SLOT_TWO_MATCH_MULT = 1.7
+
+# Рулетка
+RED_NUMBERS = {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36}
+ROULETTE_RED_MULT = 2
+ROULETTE_BLACK_MULT = 2
+ROULETTE_ZERO_MULT = 36
+
+# Кости
+DICE_HIGH_MULT = 1.9
+DICE_LOW_MULT = 1.9
+DICE_SIX_MULT = 4.5
+
+# Монетка
+COIN_MULT = 1.9
