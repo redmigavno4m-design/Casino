10e3ec5318540db2d36afe68c8ac0f9492ab48e8
redmigavno4m-design <redmigavno4m@gmail.com>
2026-09-12 03:37:01 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,27 @@ BOT_TOKEN = "8815985502:AAEAhmVxCqvQCDkudv5UnuIXmeFoLNri8Ro"
 START_BALANCE = 1000
 DAILY_BONUS = 50
 MIN_BET = 50
+
+# Блэкджек
+BJ_PAYOUT = 2  # обычная победа
+BJ_BLACKJACK_PAYOUT = 2.5  # блэкджек
+DEALER_STAND = 17
+
+# Мины
+MINES_GRID = 5
+MINES_COUNT = 3
+
+# Колесо фортуны
+WHEEL_PRIZES = [0, 50, 100, 200, 500, 1000, 250, 100]
+WHEEL_COOLDOWN = 24 * 60 * 60  # 24 часа
+
+# Уровни (опыт за игры)
+LEVELS = [
+    (0, "Новичок", 0),
+    (100, "Игрок", 100),
+    (500, "Опытный", 300),
+    (1500, "Профи", 500),
+    (5000, "Мастер", 1000),
+    (15000, "Легенда", 5000),
+    (50000, "Король", 10000),
+]
